91b202e7025db6febabe60dca2542249f4edeeb3
俞琦 <596033581@qq.com>
2017-12-05 16:27:00 +0800

    add two question
    add two question
    12.05 Tuesday

--- C-Demo/code.c
+++ C-Demo/code.c
@@ -616,3 +616,134 @@ pNode FindKthToLast(pNode list, int k, int count)
     return current;
 }
 */
+
+// 反转链表（递归 非递归两种）
+/*
+typedef struct Node {
+    int data;
+    struct Node *pNext;
+}*pNode;
+
+pNode createNode(int count)
+{
+    pNode list = (pNode)malloc(sizeof(pNode));
+    pNode current = list;
+    for (int i = 0; i < count; i ++) {
+        int value;
+        scanf("%d", &value);
+        current -> data = value;
+        if (i == count - 1) {
+            current -> pNext = NULL;
+        } else {
+            pNode next = (pNode)malloc(sizeof(pNode));
+            current -> pNext = next;
+            current = next;
+        }
+    }
+    return list;
+}
+
+
+pNode ReverseList1(pNode list)
+{
+    if (list == NULL) {
+        return NULL;
+    }
+    if (list -> pNext == NULL) {
+        return list;
+    }
+    pNode preNode = NULL;
+    pNode curNode = list;
+    while (curNode) {
+        pNode nextNode = curNode -> pNext;
+        curNode -> pNext = preNode;
+        preNode = curNode;
+        curNode = nextNode;
+    }
+    return preNode;
+}
+
+pNode ReverseListRecursivly(pNode pPre,pNode pCur)
+{
+    if(pCur == NULL)
+        return NULL;
+    if(pCur-> pNext == NULL)
+    {
+        pCur->pNext = pPre;
+        return pCur;
+    }
+    pNode pNext = pCur->pNext;
+    pCur->pNext = pPre;
+    pNode pNewHead = ReverseListRecursivly(pCur,pNext);
+    return pNewHead;
+}
+
+pNode ReverseList2(pNode list)
+{
+    return ReverseListRecursivly(NULL, list);
+}
+*/
+
+// 合并升序
+/*
+typedef struct Node {
+    int data;
+    struct Node *pNext;
+}*pNode;
+
+pNode createNode(int count)
+{
+    pNode list = (pNode)malloc(sizeof(pNode));
+    pNode current = list;
+    for (int i = 0; i < count; i ++) {
+        int value;
+        scanf("%d", &value);
+        current -> data = value;
+        if (i == count - 1) {
+            current -> pNext = NULL;
+        } else {
+            pNode next = (pNode)malloc(sizeof(pNode));
+            current -> pNext = next;
+            current = next;
+        }
+    }
+    return list;
+}
+
+pNode MergeList(pNode list1, pNode list2)
+{
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    pNode head1 = list1;
+    pNode head2 = list2;
+    pNode newHead = NULL;
+    if (head1 -> data > head2 -> data) {
+        newHead = head2;
+        newHead -> pNext = MergeList(head1, head2 -> pNext);
+    } else {
+        newHead = head1;
+        newHead -> pNext = MergeList(head1 -> pNext, head2);
+    }
+    return newHead;
+}
+
+int main(int argc, const char * argv[])
+{
+    int count1 = 0;
+    int count2 = 0;
+    scanf("%d %d", &count1, &count2);
+    pNode list1 = createNode(count1);
+    pNode list2 = createNode(count2);
+    pNode mergeList = MergeList(list1, list2);
+    pNode curNode = mergeList;
+    while (curNode) {
+        printf("%d", curNode -> data);
+        curNode = curNode -> pNext;
+    }
+    return 0;
+}
+*/
--- C-Demo/main.c
+++ C-Demo/main.c
@@ -11,8 +11,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Node {
+    int data;
+    struct Node *pNext;
+}*pNode;
+
+pNode createNode(int count)
+{
+    pNode list = (pNode)malloc(sizeof(pNode));
+    pNode current = list;
+    for (int i = 0; i < count; i ++) {
+        int value;
+        scanf("%d", &value);
+        current -> data = value;
+        if (i == count - 1) {
+            current -> pNext = NULL;
+        } else {
+            pNode next = (pNode)malloc(sizeof(pNode));
+            current -> pNext = next;
+            current = next;
+        }
+    }
+    return list;
+}
+
+pNode MergeList(pNode list1, pNode list2)
+{
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    pNode head1 = list1;
+    pNode head2 = list2;
+    pNode newHead = NULL;
+    if (head1 -> data > head2 -> data) {
+        newHead = head2;
+        newHead -> pNext = MergeList(head1, head2 -> pNext);
+    } else {
+        newHead = head1;
+        newHead -> pNext = MergeList(head1 -> pNext, head2);
+    }
+    return newHead;
+}
 
 int main(int argc, const char * argv[])
 {
+    int count1 = 0;
+    int count2 = 0;
+    scanf("%d %d", &count1, &count2);
+    pNode list1 = createNode(count1);
+    pNode list2 = createNode(count2);
+    pNode mergeList = MergeList(list1, list2);
+    pNode curNode = mergeList;
+    while (curNode) {
+        printf("%d", curNode -> data);
+        curNode = curNode -> pNext;
+    }
     return 0;
 }
+
